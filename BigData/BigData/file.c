c9b6da6e77972b68920a9560a4944b289ad18f98
nieql <915500373@qq.com>
2015-06-03 13:54:12 +0800

    file.h file.c
    file.h file.c

---
+++ BigData/BigData/file.c
@@ -0,0 +1,48 @@
+#include"qq.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/*
+	分割文件
+	path 分割文件名
+	sortpath 排序文件大小
+	getLine 获取文件每一行，返回行数据结构首地址，void * (*splitLine)(char *line)
+	elsize 行数据结构大小
+*/
+void split(char *path, char *sortpath, void* (*str2obj)(char *line), char*(*obj2str)(void *pobj),int elsize, int(*compre)(void *pa, void *pb))
+{	
+	if (NULL == path || NULL == sortpath || NULL == str2obj || elsize <= 0)
+	{
+		abort();
+	}
+	else
+	{
+		FILE *pfr = fopen(path, "r");
+		//List list = NULL;
+		//initList(&list, elsize );
+		for (int i = 0; i < elsize; i++)
+		{
+			char LineData[256] = { 0 };
+			fgets(LineData, 256, pfr);
+			
+			void *pline = str2obj(LineData);
+			//insert(&list, pline);
+
+		}
+		//sort(&list,len, elsize, compre );
+	
+
+		fclose(pfr);
+	}
+
+
+
+
+	//FILE *pr = fopen();
+	/*
+		1.获取文件行数
+		2.创建动态数组
+		3.排序
+		4.写出文件
+	*/
+}
--- BigData/BigData/qq.h
+++ BigData/BigData/qq.h
@@ -10,4 +10,7 @@ typedef struct QQ
 	char *group;
 }QQ, *PQQ;
 
-void initQQ(PQQ pqq, char *qq, char *name, int age, int sex, int auth,char *group);
+void initQQ(PQQ* pqq, char *qq, char *name, int age, int sex, int auth,char *group);
+
+void * str2obj(char *line);
+char* obj2str(void *pobj);
